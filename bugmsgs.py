6fe6ecb3b86c37dbfe4ecab9f4c5dbfea059e5df
Karina Martynava <karina.martynava@gmail.com>
2022-02-11 18:22:25 +0100

    New command : evalDAy

--- bugmsgs.py
+++ bugmsgs.py
@@ -9,6 +9,8 @@ MSG_QUESTREMIND =   ("{} !\nIl est {} HEURES ! Tout va BIEN ! ..."
                     "Allez vous émmarger sur http://quest.ajc.didierr.odns.fr/")
 MSG_QUESTCODE =     ("{}!\nLe code d'aujourd'hui est **{}** ! "
                     "Allez vous émmarger sur http://quest.ajc.didierr.odns.fr/")
+MSG_EVAL_Y =        "Oui, il faut évaluer le module {}..."
+MSG_EVAL_N =        "__**Non.**__"
 MSG_ALIVE = "https://media0.giphy.com/media/YEL7FJP6ed008/giphy.gif?cid=ecf05e47u10m4ufjbala1sc4k758gopnb7tihj8vit26aefz&rid=giphy.gif&ct=g"
 
 ERR_IDK = "https://media2.giphy.com/media/LOQJOHXx1mozCccfev/giphy.gif?cid=ecf05e47sw7v0984sua5uietrnjfzyu0n92b1c54ircp4qb1&rid=giphy.gif&ct=g"
--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from discord.ext import commands, tasks
 from datetime import date, time, datetime, timedelta
 import traceback
@@ -8,38 +9,6 @@ CODE_ROLEID = "<@&940176217896812544>"
 
 questChannel = 940625405742841866
 questChannel = 940186747638251520
-
-questModuleDuration = [
-    [date(2022, 2, 7), date(2022, 2, 7), 2925],
-    [date(2022, 2, 8), date(2022, 2, 9), 1463],
-    [date(2022, 2, 10), date(2022, 2, 10), 9262],
-    [date(2022, 2, 11), date(2022, 2, 16), 2206],
-    [date(2022, 2, 17), date(2022, 2, 21), 4611],
-    [date(2022, 2, 22), date(2022, 2, 22), 9376],
-    [date(2022, 2, 23), date(2022, 2, 23), 7902],
-    [date(2022, 2, 24), date(2022, 2, 25), 7758],
-    [date(2022, 2, 28), date(2022, 2, 28), 6647],
-
-    [date(2022, 3, 1), date(2022, 3, 4), 8960],
-    [date(2022, 3, 7), date(2022, 3,11), 8843],
-    [date(2022, 3, 14), date(2022, 3, 15), 2620],
-    [date(2022, 3, 16), date(2022, 3, 16), 8136],
-    [date(2022, 3, 17), date(2022, 3, 17), 1834],
-    [date(2022, 3, 18), date(2022, 3, 18), 9403],
-    [date(2022, 3, 21), date(2022, 3, 23), 2298],
-    [date(2022, 3, 24), date(2022, 3, 25), 7223],
-    [date(2022, 3, 28), date(2022, 3, 28), 7921],
-    [date(2022, 3, 29), date(2022, 3, 29), 5038],
-    [date(2022, 3, 30), date(2022, 4, 1), 4689],
-
-    [date(2022, 4, 4), date(2022, 4, 6), 1551],
-    [date(2022, 4, 7), date(2022, 4, 7), 2200],
-    [date(2022, 4, 8), date(2022, 4, 11), 9258],
-    [date(2022, 4, 12), date(2022, 4, 15), 7920],
-    [date(2022, 4, 19), date(2022, 4, 19), 2196],
-    [date(2022, 4, 20), date(2022, 4, 20), 9450],
-    [date(2022, 4, 21), date(2022, 4, 27), 2319],
-]
 
 class ReminderCog(commands.Cog):
 
@@ -54,11 +23,19 @@ class ReminderCog(commands.Cog):
     @staticmethod
     def getModuleCode():
         todayDay = date.today()
-        for questmodule in questModuleDuration:
+        for questmodule in questdata.QUEST_MOD_DURATION:
             if (todayDay >= questmodule[0] and todayDay <= questmodule[1]):
                 print("Today's code is actually {} ".format(questmodule[2]))
                 return questmodule[2]
         return 0000
+
+    @staticmethod
+    def isevaluationDay():
+        todayDay = date.today()
+        for questmodule in questdata.QUEST_MOD_DURATION:
+            if (todayDay == questmodule[1]):
+                return True
+        return False
     
     @staticmethod
     def secondsUtilTime(hours, minutes=0, seconds=0):
@@ -68,12 +45,21 @@ class ReminderCog(commands.Cog):
         isTommorow = 1 if (currentDatetime.time() > nextTime) else 0
         futureExec = datetime.combine(currentDatetime + timedelta(days=isTommorow), nextTime)
         return ((futureExec - currentDatetime).total_seconds())
+    
+    @commands.command()
+    async def evalDay(self, ctx):
+        try:
+            msg = bugmsgs.MSG_EVAL_Y if ReminderCog.isevaluationDay() else bugmsgs.MSG_EVAL_N
+            await ctx.channel.send(msg)
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.channel.send(bugmsgs.ERR_IDK)
 
     @commands.command()
     async def currentQuest(self, ctx):
         try:
             todaysCode = ReminderCog.getModuleCode()
-            msg = bugmsgs.ERR_IDK if todaysCode == 0000 else bugmsgs.MSG_QUESTCODE.format(CODE_ROLEID, todaysCode)
+            msg = bugmsgs.ERR_IDK if todaysCode == 0000 else bugmsgs.MSG_QUESTCODE.format(questdata.CODE_ROLEID, todaysCode)
             print(msg)
             if (datetime.today().weekday() >= 5):
                 msg = bugmsgs.ERR_NOTTODAY
@@ -84,8 +70,8 @@ class ReminderCog(commands.Cog):
 
     async def sendRemindQuest(self):
         currentHour = datetime.now().hour
-        await self.bot.get_channel(questChannel).send(bugmsgs.MSG_QUESTREMIND.format(
-            CODE_ROLEID, currentHour, ReminderCog.getModuleCode()
+        await self.bot.get_channel(questdata.QUEST_CHANNEL).send(bugmsgs.MSG_QUESTREMIND.format(
+            questdata.CODE_ROLEID, currentHour, ReminderCog.getModuleCode()
             ))
 
     @tasks.loop(hours=1)
